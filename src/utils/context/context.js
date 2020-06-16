812fb9d53cd839bfdec8084489639bfb656910f7
Willem van Duijn <willem.vanduijn@kpn.com>
2020-06-16 21:56:18 +0200

    Removed commented

--- src/utils/context/context.js
+++ src/utils/context/context.js
@@ -1,22 +1,3 @@
-// import * as tokenUtil from './token';
-// import User from '../../models/User';
-
-// const getUser = async req => {
-//   const header = req
-//     ? req.request.headers.authorization
-//     : '';
-
-//   if (header) {
-//     const token = header.replace('Bearer ', '');
-//     const decodedToken = await tokenUtil.getDecodedToken(token)
-//     return await User.findById(decodedToken.userId);
-//   }
-
-//   return null;
-// }
-
-// export { getUser };
-
 import * as tokenUtil from './token';
 import User from '../../models/User';
 
